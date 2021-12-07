7616e97ec2a4bc9d623c71417181c61977ac3a9a
Bogdan Perekhrest <bogdan.perekhrest34@gmail.com>
2021-12-07 20:20:03 +0200

    create model sprint

---
+++ db/migrate/20211207180652_create_sprints.rb
@@ -0,0 +1,11 @@
+class CreateSprints < ActiveRecord::Migration[6.1]
+  def change
+    create_table :sprints do |t|
+      t.date    :date_start,  null: false
+      t.date    :date_end,    null: false
+      t.string  :name,        null: false
+      t.string  :description, null: true
+      t.timestamps
+    end
+  end
+end
