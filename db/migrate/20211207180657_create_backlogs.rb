9ea25e1de47af27276aa64b5e23d448a512cceec
Bogdan Perekhrest <bogdan.perekhrest34@gmail.com>
2021-12-07 20:20:11 +0200

    create model backlog

---
+++ db/migrate/20211207180657_create_backlogs.rb
@@ -0,0 +1,8 @@
+class CreateBacklogs < ActiveRecord::Migration[6.1]
+  def change
+    create_table :backlogs do |t|
+      t.belongs_to :project, null: false
+      t.timestamps
+    end
+  end
+end
