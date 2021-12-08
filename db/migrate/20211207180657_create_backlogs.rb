3a7d68a84160559c3c8d4a7c6fc11ac53b427c96
Bogdan Perekhrest <bogdan.perekhrest34@gmail.com>
2021-12-08 21:48:26 +0200

    fix model and migration backlog, add frozen_string: true

--- db/migrate/20211207180657_create_backlogs.rb
+++ db/migrate/20211207180657_create_backlogs.rb
@@ -2,7 +2,6 @@ class CreateBacklogs < ActiveRecord::Migration[6.1]
   def change
     create_table :backlogs do |t|
       t.belongs_to  :project, null: false
-      t.text        :tasks, array: true, null: true
       t.timestamps
     end
   end
